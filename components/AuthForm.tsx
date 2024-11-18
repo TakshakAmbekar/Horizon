09dd3efd9d084e50b9b40d90a992cccc0d2ef050
TakshakAmbekar <ambekartakshak@gmail.com>
2024-11-18 23:40:08 +0530

    Implement Plaid link and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,6 +15,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 export default function AuthForm({ type }: { type: string }) {
   const router = useRouter();
@@ -34,9 +35,21 @@ export default function AuthForm({ type }: { type: string }) {
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
     try {
-      console.log(data);
+
       if (type === "sign-up") {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+        const newUser = await signUp(userData);
         setUser(newUser);
       }
       if (type === "sign-in") {
@@ -78,7 +91,9 @@ export default function AuthForm({ type }: { type: string }) {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4"></div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary"/>
+        </div>
       ) : (
         <>
           <Form {...form}>
@@ -128,7 +143,7 @@ export default function AuthForm({ type }: { type: string }) {
                   <div className="flex gap-4">
                     <CustomInput
                       control={form.control}
-                      name="dob"
+                      name="dateOfBirth"
                       label="Date of Birth"
                       placeholder="YYYY-MM-DD"
                     />
--- lib/utils.ts
+++ lib/utils.ts
@@ -207,10 +207,10 @@ export const authFormSchema = (type: string) =>
     city:
       type === "sign-in" ? z.string().optional() : z.string().min(2).max(20),
     state:
-      type === "sign-in" ? z.string().optional() : z.string().min(2).max(20),
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
     postalCode:
-      type === "sign-in" ? z.string().optional() : z.string().min(6).max(6),
-    dob: type === "sign-in" ? z.string().optional() : z.string().min(3),
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(10),
+    dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
     ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
     //both
     email: z.string().email(),
